e56b7302d81abf8265b4d5e5accc986c014d85b3
Jhonny0102 <fjhon868@gmail.com>
2024-05-21 13:25:18 +0200

    Add project files.

---
+++ MvcCoreElastiCacheAWS/Controllers/CochesController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcCoreElastiCacheAWS.Models;
+using MvcCoreElastiCacheAWS.Repository;
+
+namespace MvcCoreElastiCacheAWS.Controllers
+{
+    public class CochesController : Controller
+    {
+        private RepositoryCoches repo;
+        public CochesController(RepositoryCoches repo)
+        {
+            this.repo = repo;
+        }
+        public IActionResult Index()
+        {
+            List<Coche> coches = this.repo.GetCoches();
+            return View(coches);
+        }
+
+        public IActionResult Details(int idcoche)
+        {
+            Coche coche = this.repo.FindCoche(idcoche);
+            return View(coche);
+        }
+
+    }
+}
---
+++ MvcCoreElastiCacheAWS/Helpers/HelperCacheRedis.cs
@@ -0,0 +1,21 @@
+﻿using StackExchange.Redis;
+
+namespace MvcCoreElastiCacheAWS.Helpers
+{
+    public class HelperCacheRedis
+    {
+        public static Lazy<ConnectionMultiplexer> CreateConnection = new Lazy<ConnectionMultiplexer>(() =>
+        {
+            //AQUI IRA NUESTRA CADENA DE CONECCION
+            return ConnectionMultiplexer.Connect("");
+        });
+
+        public ConnectionMultiplexer Connection
+        {
+            get
+            {
+                return CreateConnection.Value;
+            }
+        }
+    }
+}
